7fe433dec112e5feabe2fb4121babc2aba30cbd1
Clay Dugo <claydugo@gmail.com>
2020-10-19 16:02:32 -0400

    populate data and update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CSC455 Final Project - Bank Database
+A mock banking database with a web front end written in python.
+
+#### Notes to group.
+
+If you are connecting to mySQL normally through satoshi you can run `use narayanFall2020group3;` and the database should currently be operating as intended.
+
+If it is not you can clone this repo to your satoshi directory, then run `source bank.sql` followed by `source insertData.sql` to rebuild the database.
 
 
--- insertData.sql
+++ insertData.sql
@@ -1,10 +1,23 @@
 SET FOREIGN_KEY_CHECKS=0;
-insert into account values(1337 , 100000, 'Admin');
-insert into account values(1 , 0, 'test');
-insert into account values(123 , 100, 'User');
+insert into account values(1337, 100000, 'Admin');
+insert into account values(201, 30212, 'User');
+insert into account values(202, 4002, 'User');
+insert into account values(204, 875, 'User');
+insert into account values(205, 320, 'User');
+insert into account values(206, 7832, 'User');
+insert into account values(207, 60, 'User');
+insert into account values(208, 621435, 'User');
+insert into account values(209, 2217, 'User');
 
-insert into customer values(3, 'John Doe', 'Wilmington', 123);
-insert into customer values(1, 'Admin User', 'Nowhere', 1337);
+insert into customer values(301, 'Truman Burbank', 'Montauk', 201);
+insert into customer values(302, 'Hannah Burbank', 'Montauk', 202);
+insert into customer values(303, 'Cherita Chen', 'Los Angeles', 203);
+insert into customer values(304, 'Jim Cunningham', 'Los Angeles', 204);
+insert into customer values(305, 'Nathan Young', 'Los Angeles', 205);
+insert into customer values(306, 'Curtis Donovan', 'Los Angeles', 206);
+insert into customer values(307, 'Lawrence Gordon', 'Covington', 207);
+insert into customer values(308, 'Amy Dunne', 'Covington', 208);
+insert into customer values(309, 'Annie Wilkes', 'Covington', 209);
 
 insert into employee values(1201 , 'Joel Barish', 'Montauk', '7045558896');
 insert into employee values(1202 , 'Clementime Kruczynski', 'Montauk', '7045557432');
@@ -16,6 +29,8 @@ insert into branch values('Alpha', 'Wilmington');
 insert into branch values('Bravo', 'Covington');
 insert into branch values('Charlie', 'Los Angeles');
 
-insert into loan values(101, 150000, 10, 1000, 30, 123);
-insert into loan values(102, 1000000, 25, 10000, 15, 1337);
+insert into loan values(101, 150000, 10, 1000, 30, 201);
+insert into loan values(102, 10000, 12, 833, 1, 202);
+insert into loan values(103, 3000, 40, 250, 30, 203);
+insert into loan values(104, 1000000, 5, 50000, 2, 208);
 SET FOREIGN_KEY_CHECKS=1;
