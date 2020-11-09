580b45bbad163417c5ced0dacf4fef4e2af00054
Clay Dugo <claydugo@gmail.com>
2020-11-09 10:50:20 -0500

    menu refactoring, work still needed

--- menu.py
+++ menu.py
@@ -21,10 +21,10 @@ class Menu:
 
 
 def mainMenu():
-    opt = ['Get Balance', 'Deposit', 'List Employees']
+    opt = ['Manage Accounts', 'Manage Employees', 'Placeholder']
     main = Menu('Command Line Bank', opt, 'placeholder')
     print(main)
-    uSelec = input(f'Select an option from 1 - {len(opt)}\n')
+    uSelec = int(input(f'Select an option from 1 - {len(opt)}\n'))
     menuNav(uSelec)
 
 
@@ -37,27 +37,37 @@ def menuNav(uSelec):
         except ValueError:
             print('Invalid Entry, navigating back to main menu')
             time.sleep(1)
+            mainMenu()
 
 def menuExec(uSelec):
-    if uSelec == '1':
-        getBalance()
+    if uSelec == 1:
+        accountManagement()
         return
-    elif uSelec == '2':
-        mainMenu()
-        print('Would execute Deposit')
-        return
-        # Deposit
-    elif uSelec == '3':
+    elif uSelec == 2:
         listEmployees()
         return
-    elif uSelec == '4':
+        # Deposit
+    elif uSelec == 3:
         mainMenu()
-        print('Would execute yada')
-        #yada
         return
+    elif uSelec == 4:
+        getName()
+        return
+    elif uSelec == 5:
+        getBalance()
+        return
+    elif uSelec == 6:
+        getCustomerCity()
     else:
         print('else lol')
 
+def accountManagement():
+    menuBuffer = 3
+    opt = ['Get Name', 'Get Balance', 'Get City']
+    account = Menu('Command Line Bank', opt, 'placeholder')
+    print(account)
+    uSelec = int(input(f'Select an option from 1 - {len(opt)}\n'))
+    menuNav(menuBuffer+uSelec)
 
 def navBack():
     ans = input('Would you like to navigate back to the main menu? Y/N\n')
@@ -65,6 +75,7 @@ def navBack():
         mainMenu()
         return
     elif ans == 'N' or ans == 'n':
+        time.sleep(1)
         exit()
     else:
         navBack()
@@ -79,15 +90,53 @@ def initializeDB():
     )
     return mydb
 
+def getName():
+    accID = input('Please enter your account ID\n')
+    mydb = initializeDB()
+    c = mydb.cursor()
+    c.execute(f'select cust_name from customer,account where customer.acc_id = {accID} and account.acc_id = {accID}')
+    try:
+        res = c.fetchall()
+        name = res[0][0]
+        print(name)
+        navBack()
+    except:
+        print(f'No account found with the ID: {accID}')
+        time.sleep(2)
+        mainMenu()
+
 def getBalance():
     accID = input('Please enter your account ID\n')
     mydb = initializeDB()
     c = mydb.cursor()
     c.execute(f'select balance from account where acc_id = {accID}')
-    res = c.fetchall()
-    bal = res[0][0]
-    print(bal)
-    navBack()
+    try:
+        res = c.fetchall()
+        bal = res[0][0]
+        print(bal)
+        navBack()
+    except:
+        print(f'No account found with the ID: {accID}')
+        time.sleep(2)
+        mainMenu()
+
+
+def getCustomerCity():
+    accID = input('Please enter your account ID\n')
+    mydb = initializeDB()
+    c = mydb.cursor()
+    c.execute(f'select cust_city from customer,account where customer.acc_id = {accID} and account.acc_id = {accID}')
+    try:
+        res = c.fetchall()
+        print(res[0][0])
+        return
+    except:
+        print(f'No account found with the ID: {accID}')
+        time.sleep(2)
+        mainMenu()
+
+
+
 
 def listEmployees():
     mydb = initializeDB()
